6bb1777bb3c102754f4d6d715bf505f7fb195304
Kris Champ <krischamp@gmail.com>
2015-12-30 16:41:55 +0100

    add clean gulp file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,92 +1,180 @@
 'use strict';
- 
+
+// -------------   INIT VAR   ------------------
+// 
 var gulp = require('gulp');
 
 var sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps'),
-    livereload = require('gulp-livereload'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    minifycss = require('gulp-minify-css'),
+    jade = require('gulp-jade'),
+    watch = require('gulp-watch'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    // autoprefixer = require('gulp-autoprefixer'),
+    // rename = require('gulp-rename'),
     connect = require('gulp-connect');
-    
 
+    
+// -------------   PATHS   ------------------
+// 
 var paths = {
+  /* js files */
   'js': {
-    in: ['./assets/js/vendors/jquery.js',
-        './assets/js/vendors/bootstrap.js',
-        './assets/js/vendors/jquery.easing.min.js',
-        './assets/js/vendors/jquery.fittext.js',
-        './assets/js/vendors/wow.min.js',
-        './assets/js/vendors/creative.js',
-        './assets/js/scripts/*.js'],
-    out: 'scripts.js',
-    folderOut: './assets/js/'
+    inSrc: [
+      './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+      './bower_components/jquery/dist/jquery.js',
+      './src/js/custom/*.js'
+    ],
+    output: {
+      file: 'scripts.js',
+      folder: './src/js/'
+    }
   },
-  'style': {
-    in: ['./assets/scss/*.scss'],
-    folderOut: './assets/css/'
+  /* style scss */
+  'scss': {
+    inSrc: ['./src/styles/**/*.scss'],
+    output: {
+      folder: './src/styles/'
+    }
+  },
+  /* jade files */
+  'jadeFiles': {
+    templates: [
+      './src/**/*.jade'
+    ]
+  },
+  /* html generation */
+  'html': {
+    srcFolder: './src/',
+    srcFiles: './src/**/*.html'
   }
 
 };
 
+
+//-----------   SERVER   ---------------------
+// 
 gulp.task('server:start', function() {
   connect.server({
-    port: 8000
+    port: 8000,
+    root: './src',
   });
-  // server close ?
 });
  
 
-
- 
+// ----------   SASS   -----
+/* sass DEV */
 gulp.task('sass:dev', function () {
-  gulp.src(paths.style.in)
+  gulp.src(paths.scss.inSrc)
 	.pipe(sourcemaps.init())
-	.pipe(sass({includePaths: [ './assets/scss', 
-                              '/bower_components/bootstrap-sass/assets/stylesheets/bootstrap/']}))
-	// .pipe(sourcemaps.write())
-  .pipe(gulp.dest(paths.style.folderOut))
+	.pipe(sass())
+  .pipe(gulp.dest(paths.scss.output.folder))
   .pipe(sass.sync().on('error', sass.logError))
   .pipe(livereload());
 });
  
-
+/* sass BUILD */
 gulp.task('sass:build',function () {
-  gulp.src(paths.style.in)
+  gulp.src(paths.scss.inSrc)
     .pipe(sass())
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(minifycss())
-    .pipe(gulp.dest(paths.style.folderOut));
+    .pipe(gulp.dest(paths.scss.output.folder));
 });
 
+
+// ----------   JSCONCAT   -----
+/*  js scripts concatenation DEV */
 gulp.task('jsconcat:dev', function() {
-  return gulp.src(paths.js.in)
-    .pipe(concat(paths.js.out))
-    .pipe(gulp.dest(paths.js.folderOut))
+  return gulp.src(paths.js.inSrc)
+    .pipe(concat(paths.js.output.file))
+    .pipe(gulp.dest(paths.js.output.folder))
     .pipe(livereload());
 });
 
+/* js scripts concatenation BUILD */
 gulp.task('jsconcat:build', function() {
-  return gulp.src(paths.js.in)
-    .pipe(concat(paths.js.out))
+  return gulp.src(paths.js.inSrc)
+    .pipe(concat(paths.js.output.file))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.js.folderOut));
+    .pipe(gulp.dest(paths.js.output.folder));
 });
 
-gulp.task('sass:watch', function () {
+
+//----------- JADE -> HTML -------------------
+/* html generation from jade files */
+gulp.task('jadeHtml', function() {
+  gulp.src(paths.jadeFiles.templates)
+    .pipe(jade({
+      locals: paths.jadeFiles.templates
+    }))
+    .pipe(gulp.dest(paths.html.srcFolder))
+    .pipe(livereload());
+});
+
+
+//-----------   WATCHERS   ---------------------
+/* general watch task */
+gulp.task('watch', [
+  'watch:sass',
+  'watch:js',
+  'watch:jadeHtml'
+]);
+
+/* SASS */
+gulp.task('watch:sass', function () {
   livereload.listen();
-  gulp.watch(paths.style.in, ['sass:dev']);
-  gulp.watch('./assets/js/**/*.js', ['jsconcat:dev']);
+  gulp.watch(paths.scss.inSrc, ['sass:dev']);
+});
+
+/* JS */
+gulp.task('watch:js', function () {
+  livereload.listen();
+  gulp.watch(paths.js.inSrc, ['jsconcat:dev']);
+});
+
+/* JADE -> HTML */
+gulp.task('watch:jadeHtml', function () {
+  livereload.listen();
+  gulp.watch(paths.jadeFiles.templates, ['jadeHtml']);
   /* Trigger a live reload on any html changes */
-  gulp.watch('*.html').on('change', livereload.changed);
+  gulp.watch(paths.html.srcFiles).on('change', livereload.changed);
 });
 
-gulp.task('serve', ['server:start', 'sass:watch', 'jsconcat:dev'],function () {
+
+//-----------   CLEAN   ---------------------
+/* Clean 'dist' folder before generating new one with copy task */
+gulp.task('cleaning', function () {
+  return del([
+    'dist/**'
+  ]);
 });
 
-gulp.task('build', ['sass:build', 'jsconcat:build'],function () {
+
+//-----------   COPY   ---------------------
+/*  generate a new distribution version */
+gulp.task('copy', ['cleaning'], function() {
+  gulp.src([
+    './src/**/*.html', 
+    './src/**/scripts.js',
+    './src/**/style.css',
+    './src/media/*',
+    './src/*.png',
+    './src/*.jpg',
+    './src/*.svg',
+  ])
+  .pipe(gulp.dest('./dist'));
 });
 
+
+// ----------   RUN tasks   ------------------
+/* default task (watch & serve) */
+gulp.task('serve', ['server:start', 'watch'],function () {
+});
+
+/* distribution task */
+gulp.task('dist', ['sass:build', 'jsconcat:build', 'jadeHtml', 'copy'],function () {
+});
